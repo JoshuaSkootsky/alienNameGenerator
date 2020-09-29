143929a923588ddd8a53348fbe3740776564f0ad
Joshua Skootsky <joshua.skootsky@gmail.com>
2020-09-29 17:51:51 +0000

    script works now

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
-function alienNameMaker(name, encoder) {
-  // get name from form 
-  // encode it into alien name with the encoder mapping
-
-}
-
 const textField = document.getElementById('comment');
 const result = document.getElementById('result');
+// add event listener to textField
+textField.addEventListener('input', setResult);
 
-function setResult() {
-   result.innerHTML = "result goes here";
+function chooseRandom(array) {
+  const max = array.length;
+  return array[getRandomInt(max)];
 }
 
-textField.addEventListener('input', setResult);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function setResult() {
+  const name = textField.value;
+   result.innerHTML = alienNameMaker(name, encoder)
+}
+
+function alienNameMaker(name, encoder) {
+  // given a name string
+  // encode it into alien name with the encoder mapping
+  let alienName = '';
+  for (let i = 0; i < name.length; i++) {
+    const char = name[i].toLowerCase();
+
+    const alienCharArray = encoder[char];
+    // this char array may be undefined
+    if (alienCharArray) {
+      const alienChar = chooseRandom(alienCharArray);
+       alienName = alienName + alienChar;
+    }
+
+    // choose an alien char from the array
+    
+
+    // skip non-truthy values of alienChar
+    
+   
+  }
+  return alienName;
+}
 
 /* function doThing(){
    alert('Horray! Someone wrote something!');
 } */
 
-// this object maps each letter of the alphabet to an alien letter
+// this object maps each letter of the alphabet to an array of alien letters
 const encoder = {
-  a: "å",
-  b: "bo",
-  c: "krax",
-  d: "du",
-  e: "i-i",
-  f: "wa",
-  g: "k",
-  h: "ℍ",
-  i: "-hi",
-  j: "j",
-  k: "k`",
-  l: "la",
-  m: "mep",
-  n: "in",
-  o: "ȫ ",
-  p: "plorg",
-  q: "qlax",
-  r: "ar",
-  s: "sork",
-  t: "trag",
-  u: "oo",
-  v: "vyrt",
-  w: "weewo",
-  x: "⸖",
-  y: "yamski-af'liax",
-  z: "Ⱬ"
-};
+    a: ["å", "œ"],
+    b: ["boo", "bb"],
+    c: ["krax", "ç"],
+    d: ["du", "∂"],
+    e: ["i-i", "é"],
+    f: ["wa", "ƒ"],
+    g: ["k", "˚kay"],
+    h: ["ℍ", "^"],
+    i: ["-hi", "î"],
+    j: ["j", "∆"],
+    k: ["k`", "˚"],
+    l: ["la", "¬"],
+    m: ["meep", "µ"],
+    n: ["in", "ñ"],
+    o: ["ȫ ", "ø"],
+    p: ["plorg", "π"],
+    q: ["qlax", "kyo"],
+    r: ["ar", "rar"],
+    s: ["smork", "ß"],
+    t: ["trarg", "†"],
+    u: ["oo", "ü"],
+    v: ["vyort", "√"],
+    w: ["weewo", "∑"],
+    x: ["⸖", "≈"],
+    y: ["yamska-i'liax", "¥"],
+    z: ["Ⱬ", "Ω"]
+  };
